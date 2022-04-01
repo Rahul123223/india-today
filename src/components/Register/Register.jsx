fc6ba48e5d714c10a211a49f0f67890bd3e2a921
Saarah26 <sakshirai727@gmail.com>
2022-04-02 02:20:13 +0530

    hlo

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import { unstable_HistoryRouter } from "react-router-dom";
+import { Routes,Route} from "react-router-dom";
 import "../styles/register.css";
 
 
@@ -10,7 +10,25 @@ export const Register = () => {
        email:"",
        password:""
     });
-    const handleInput = () => {
+    const [records ,setRecords] = useState([]);
+
+    const handleInput = (e) => {
+        const className = e.target.className;
+        const value = e.target.value;
+        console.log(className , value);
+
+        setuserRegistration({...userRegistration,[className]:value});
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newRecord = {...userRegistration,id:new Date().getTime().toString() }
+        console.log(records);
+
+        setRecords([...records,newRecord]);
+        console.log(records);
 
     }
 
@@ -27,39 +45,56 @@ export const Register = () => {
    
    <div className="icon">
 
-   <a href="https://auth.indiatoday.in/saml_login/twitter/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-twitter"> Twitter</a>
-   <a href="https://auth.indiatoday.in/saml_login/facebook/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-facebook"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a>
-   <a href="https://auth.indiatoday.in/saml_login/google/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-google"> Google</a>
+   <a href="https://auth.indiatoday.in/saml_login/twitter/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-twitter"> 
+    <div className="hu"> <img src="https://th.bing.com/th/id/OIP.WlVEYN86Ndj4KZiGf4zvCAHaHa?w=180&h=180&c=7&r=0&o=5&pid=1.7 "  alt="" className="zz"/><div className="twitter">Twitter</div></div>
+       </a>
+   <a href="https://auth.indiatoday.in/saml_login/facebook/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-facebook">
+   <div className="hu"> <img src="https://th.bing.com/th/id/OIP.opWSg0C5fPLAUyJzE9e_VwHaHa?w=217&h=216&c=7&r=0&o=5&pid=1.7 "  alt="" className="zz"/><span className="fb" >Facebook</span></div>
+   </a>
+   <a href="https://auth.indiatoday.in/saml_login/google/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-google"> 
+   <div className="hu"> <img src="https://th.bing.com/th/id/OIP.1Nl9otEDi23qnVEQXPJuKQHaHa?w=162&h=180&c=7&r=0&o=5&pid=1.7 "  alt="" className="zz"/><span className="ggl" >Google</span></div>
+   </a>
    </div>
    <h3>OR</h3>
     <h3 className="one"> Sign Up using your Email Id/Mobile No.</h3>
     </div>
+    
+    <form action="" onSubmit = {handleSubmit}>
+
     <div class="input1">
+
         <select className="country">
             <option>India</option>
             <option>USA</option>
             <option>UK</option>
+            <option value="">Other country</option>
+        </select> 
 
-
-         </select> 
-        <input placeholder="Enter Your Full Name"  className="Name"  value="userRegistration.username "  onChange={handleInput} id="username"/>
+        <input placeholder="Enter Your Full Name"  className="name"  value={userRegistration.name} onChange={handleInput} id="name"/>
     </div>
+
     <div class="input2">
-    <input placeholder="Enter Your Email"  className="Name"  value={userRegistration.email}  onChange={handleInput} id="email"/>
-        <input placeholder=" * Password"  className="Name"    value={userRegistration.password}  onChange={handleInput} id="password"/>
+    <input placeholder="Enter Your Email"  className="email"  value={userRegistration.email}  onChange={handleInput} id="email"/>
+        <input placeholder=" * Password"  className="password"    value={userRegistration.password}  onChange={handleInput} id="password"/>
     </div>
+
+    </form>
+
+
     <div className="box">
-    <input className="inpt" type="text" />
+    <input className="inpt" type="checkbox" />
     <span className="span"> I agree to the SSO login privacy terms and I warrant that I am above 16 years of age. <span className="xx">*</span></span>
 
-    <input className="inpt" type="text" />
+    <input className="inpt" type="checkbox" />
     <span className="span"> I agree to the processing of my personal information for profiling such as My Credits,
-         Follow activity , Personalised recommendations and Advertisements. </span>
+         Follow activity , Personalised recommendations and Advertisements. </span><br/>
+        
 
-         <input className="inpt" type="text" />
+         <input className="inpt" type="checkbox" />
     <span className="span">I wish to receive custom communications i.e. Emails, newsletters, SMS, notifications from India Today based on my interests and my activities on the website. </span>
 
          </div>
+         
          <button className="ss" type="submit">Sign Up</button>
 
          <div className="tt"> 
--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -14,22 +14,59 @@ export const Signin = () => {
   <div className="deco">
   <h3>Sign In Via</h3>
    
-   <div className="icon">
+  <div className="icon">
 
-   <a href="https://auth.indiatoday.in/saml_login/twitter/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-twitter"> Twitter</a>
-   <a href="https://auth.indiatoday.in/saml_login/facebook/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-facebook"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a>
-   <a href="https://auth.indiatoday.in/saml_login/google/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-google"> Google</a>
+   <a href="https://auth.indiatoday.in/saml_login/twitter/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-twitter"> 
+    <div className="hu"> <img src="https://th.bing.com/th/id/OIP.WlVEYN86Ndj4KZiGf4zvCAHaHa?w=180&h=180&c=7&r=0&o=5&pid=1.7 "  alt="" className="zz"/><div className="twitter">Twitter</div></div>
+       </a>
+   <a href="https://auth.indiatoday.in/saml_login/facebook/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-facebook">
+   <div className="hu"> <img src="https://th.bing.com/th/id/OIP.opWSg0C5fPLAUyJzE9e_VwHaHa?w=217&h=216&c=7&r=0&o=5&pid=1.7 "  alt="" className="zz"/><span className="fb" >Facebook</span></div>
+   </a>
+   <a href="https://auth.indiatoday.in/saml_login/google/aHR0cHM6Ly93d3cuaW5kaWF0b2RheS5pbi8jc3NvbG9naW49MQ==" class="sso-google"> 
+   <div className="hu"> <img src="https://th.bing.com/th/id/OIP.1Nl9otEDi23qnVEQXPJuKQHaHa?w=162&h=180&c=7&r=0&o=5&pid=1.7 "  alt="" className="zz"/><span className="ggl" >Google</span></div>
+   </a>
    </div>
+
     <h3 className="one"><b>Or Sign In using your Email or Mobile no.</b></h3>
     </div>
-    <div className="ary1">
-        <div className="aa">
-            <img className="bb"></img>
-        </div>
-    <input placeholder="Enter Your Email/Mobile" type="text" id="cc"  ></input>
+    <div className="kk">
+    <img src="https://th.bing.com/th/id/OIP.ZukVvz62s21LSc4we5WeagHaHa?w=185&h=181&c=7&r=0&o=5&pid=1.7"  alt="" className="pic"/>
+    <input placeholder="Enter Your Email/Mobile" type="text" className="cc"  ></input>
+    </div>
+
+    <div className="kk1">
+    <img src="https://th.bing.com/th/id/OIP.fC2yOxAzBsS2Kv5Vo2jgegHaHa?w=162&h=180&c=7&r=0&o=5&pid=1.7 "  alt="" className="pic"/>
+    <input placeholder="Enter Your Password" type="text" className="cc"  ></input>
+    </div>
+    <div className="me">
+        <input type="checkbox" />
+        <span>Remember me</span>
+    </div>
+    <div className="blue">
+        <p3 className="b"><b>Forget Password? </b></p3>
+        <p3 className="b"><b>Login via OTP</b></p3>
+    </div>
+    <button className="ss" type="submit">Log In</button>
+
+       
+   <div className="footer">
+       <div className="foot1">
+           <p1 className="network">One Network One Account</p1>
+         <div className="imgy"><img  className ="imgx"  src="https://smedia2.intoday.in/aajtak/aajtakhd/images/aajtakHd_logo.png?bypass=true"/></div> 
+        <div className="imgy">  <img  className ="imgx" src="https://akm-img-a-in.tosshub.com/sites/all/themes/itg/logo.png?576"/></div>
+           <div className="imgy"><img  className ="imgx" src="https://akm-img-a-in.tosshub.com/sites/btmt/businesstoday/resources/css/images/btnav-newlogo.png?size=48:21"/></div>
+       
+       </div>
+       <div className="foot2">
+           <p1 className="last">Copyright © 2022 Living Media India Limited. For reprint rights: Syndications Today</p1>
+       </div>
+   </div>
+  
+
+    
 
 
-</div>
+
     </div>
     
   
